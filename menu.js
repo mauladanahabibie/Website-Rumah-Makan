78765b662c66efc084ce3ea406dd271ae21492d5
Mauladana Habibie <138235168+mauladanahabibie@users.noreply.github.com>
2024-06-08 17:11:19 +0700

    fix some error

--- menu.js
+++ menu.js
@@ -16,30 +16,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}// Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document smoothly
-mybutton.addEventListener("click", function() {
-  smoothScrollToTop();
-});
-
-function smoothScrollToTop() {
-  var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
-
-  if (currentPosition > 0) {
-    window.requestAnimationFrame(smoothScrollToTop);
-    window.scrollTo(0, currentPosition - currentPosition / 8);
-  }
 }
